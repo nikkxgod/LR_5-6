140c0942d457a9fb2748a24f39d4e1806f84aae1
nikkxgod <100446613+nikkxgod@users.noreply.github.com>
2023-10-05 09:41:14 +0300

    Add files via upload

---
+++ 1.2.pas
@@ -0,0 +1,20 @@
+﻿var A:array[1..20] of integer;i,z,b,q,s,p:integer;
+  begin 
+    writeln('Введите нижнюю границу поиска');
+    read(z);
+    writeln('Введите нижнюю границу поиска');
+    read(b);
+    q:=0;
+    s:=0;
+    p:=1;
+    for i:=1 to 20 do begin
+      A[i]:=random(115)-22;
+      if (i mod 2 <> 0) and (A[i] mod 2 = 0) then q := q+1;
+      if (i mod 2 <> 0) then p:=p*A[i];
+      if (A[i]>=z) and (A[i]<=b)then s:=s+A[i];
+     end;
+     writeln(A);
+     writeln(q);
+     writeln(p);
+     writeln(s);
+  end.
