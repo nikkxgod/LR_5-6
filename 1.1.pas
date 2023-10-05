7b3ffbeff6fb97982e86fa6fdfd3dc0c09646a0a
nikkxgod <100446613+nikkxgod@users.noreply.github.com>
2023-10-05 09:12:31 +0300

    Add files via upload

---
+++ 1.1.pas
@@ -0,0 +1,11 @@
+﻿var A:array[1..20] of integer;i,x:integer;
+  begin
+    writeln('Заполните массив');
+    for i:=1 to 20 do begin
+      read(x);
+      if (x>0) then x:=0;
+      if (x<0) then x:=sqr(x);
+      A[i]:=x;
+    end;
+    write(A)
+  end.
