d80a2e610d193c397a36c6982849253533f96404
nikkxgod <100446613+nikkxgod@users.noreply.github.com>
2023-10-10 21:39:19 +0300

    Add files via upload

---
+++ 6.pas
@@ -0,0 +1,12 @@
+﻿var A:array[1..20] of integer;flag,i:integer;
+  begin
+    flag := 0;
+    for i:=1 to 20 do
+       A[i]:=random(100)-100;
+    for i:=1 to 19 do begin
+        if A[i]>A[i+1] then flag:=flag+1;
+        end;
+    if flag <> 0 then writeln('Не упорядочен')
+    else writeln('Упорядочен');
+    writeln(A);
+    end.
