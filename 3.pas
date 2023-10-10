33c856ded50f3ae96cb3b507e99681489bf5dd36
nikkxgod <100446613+nikkxgod@users.noreply.github.com>
2023-10-10 11:20:50 +0300

    Add files via upload

---
+++ 3.pas
@@ -0,0 +1,24 @@
+﻿var A:array[1..20] of integer;i,max,max_ind,min,min_ind,krat5:integer;
+  begin 
+    max := -100;
+    min := 100;
+    for i:=1 to 20 do begin
+      A[i]:=random(118)-52;
+      if A[i]>max then begin 
+      max:=A[i];
+      max_ind:=i
+      end;
+      if A[i]<min then begin
+        min:=A[i];
+        min_ind:=i;
+      end;
+      if A[i] mod 5 = 0 then
+        krat5 := i;
+    end;
+    writeln('Исходный массив: ',A);
+    writeln('Максимальное: ',max);
+    writeln('Индекекс максимального элемента: ',max_ind);
+    writeln('Минимальный элемент: ',min);
+    writeln('Индекс минимального элемента: ',min_ind);
+    writeln('Индекс последнего элемента кратного 5: ',krat5)
+  end.
---
+++ 4.pas
@@ -0,0 +1,17 @@
+﻿var A:array[1..30] of integer;i,size,B_ind:integer;B:array of integer;
+  begin
+    size:=0;
+    B_ind:=-1;
+    for i:=1 to 30 do begin
+      A[i]:=random(167)-99;
+      if A[i] mod 2 = 0 then size:=size+1;
+    end;
+    SetLength(B,size);
+    for i:=1 to 30 do begin
+      if A[i] mod 2 = 0 then begin B_ind:=B_ind+1;
+      B[B_ind]:=A[i];
+      end;
+      end;
+    writeln(A);
+    writeln(B);
+    end.
